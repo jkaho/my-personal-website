6dcdac4ae5250944c469c7aca9caff87223ff6e6
jkaho <72959522+jkaho@users.noreply.github.com>
2021-01-26 01:33:46 +1100

    Modify js & css to change background on dark btn click, disable other theme btns on click

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,21 +7,61 @@ var doomBtn = $("#doom-btn");
 
 dayBtn.on("click", function(event){
     event.preventDefault();
+
+    dawnBtn.attr("disabled", true);
+    duskBtn.attr("disabled", true);
+    darkBtn.attr("disabled", true);
+    doomBtn.attr("disabled", true);
+
     background.attr("class", "day");   
     setTimeout(function(){
         background.attr("style", 
         "background: url('./assets/images/general/bg-day.png'); background-size: contain; background-repeat: no-repeat; width: 100%; height: 0; padding-top: 75%;"
         );
+        dawnBtn.attr("disabled", false);
+        duskBtn.attr("disabled", false);
+        darkBtn.attr("disabled", false);
+        doomBtn.attr("disabled", false);
     }, 2000) 
 })
 
 duskBtn.on("click", function(event){
     event.preventDefault();
+
+    dawnBtn.attr("disabled", true);
+    dayBtn.attr("disabled", true);
+    darkBtn.attr("disabled", true);
+    doomBtn.attr("disabled", true);
+
     background.attr("class", "dusk");
     setTimeout(function(){
         background.attr("style", 
         "background: url('./assets/images/general/bg-dusk.png'); background-size: contain; background-repeat: no-repeat; width: 100%; height: 0; padding-top: 75%;"
         );
+        dawnBtn.attr("disabled", false);
+        dayBtn.attr("disabled", false);
+        darkBtn.attr("disabled", false);
+        doomBtn.attr("disabled", false);
+    }, 2000) 
+})
+
+darkBtn.on("click", function(event){
+    event.preventDefault();
+
+    dawnBtn.attr("disabled", true);
+    duskBtn.attr("disabled", true);
+    dayBtn.attr("disabled", true);
+    doomBtn.attr("disabled", true);
+
+    background.attr("class", "dark");
+    setTimeout(function(){
+        background.attr("style", 
+        "background: url('./assets/images/general/bg-dark.png'); background-size: contain; background-repeat: no-repeat; width: 100%; height: 0; padding-top: 75%;"
+        );
+        dawnBtn.attr("disabled", false);
+        duskBtn.attr("disabled", false);
+        dayBtn.attr("disabled", false);
+        doomBtn.attr("disabled", false);
     }, 2000) 
 })
 
