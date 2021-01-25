af77c9d6e6ca5f42dd0d45fdf4f7898b0191a055
jkaho <72959522+jkaho@users.noreply.github.com>
2021-01-26 01:26:37 +1100

    Modify js & css to change background on dusk btn click, fix 'change from dawn' bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,21 @@ var doomBtn = $("#doom-btn");
 
 dayBtn.on("click", function(event){
     event.preventDefault();
-    background.attr("class", "day");    
+    background.attr("class", "day");   
+    setTimeout(function(){
+        background.attr("style", 
+        "background: url('./assets/images/general/bg-day.png'); background-size: contain; background-repeat: no-repeat; width: 100%; height: 0; padding-top: 75%;"
+        );
+    }, 2000) 
+})
+
+duskBtn.on("click", function(event){
+    event.preventDefault();
+    background.attr("class", "dusk");
+    setTimeout(function(){
+        background.attr("style", 
+        "background: url('./assets/images/general/bg-dusk.png'); background-size: contain; background-repeat: no-repeat; width: 100%; height: 0; padding-top: 75%;"
+        );
+    }, 2000) 
 })
 
