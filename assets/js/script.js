b6a2a024e47423dc0af3da646275d99e9579ceca
jkaho <72959522+jkaho@users.noreply.github.com>
2021-01-27 00:36:23 +1100

    Modify css & js to add animation to background of background image

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,17 +13,18 @@ dawnBtn.on("click", function(event){
 
     themeBtnDiv.css("background", "rgb(177, 75, 75)");
     footerDiv.css("background", "#FFE8EF");
-    bannerDiv.css("background", "#FFE8EF");
     dayBtn.attr("disabled", true);
     duskBtn.attr("disabled", true);
     darkBtn.attr("disabled", true);
     doomBtn.attr("disabled", true);
 
     background.attr("class", "dawn");   
+    bannerDiv.attr("class", "dawn-outer");
     setTimeout(function(){
         background.attr("style", 
-        "background: url('./assets/images/general/bg-dawn.png'); background-size: contain; background-repeat: no-repeat; width: 100%; height: 0; padding-top: 75%;"
+        "background: url('./assets/images/general/bg-dawn.png'); background-size: contain; background-repeat: no-repeat; width: 80%; height: 0; padding-top: 60%;"
         );
+        bannerDiv.css("background", "#FFE8EF");
         dayBtn.attr("disabled", false);
         duskBtn.attr("disabled", false);
         darkBtn.attr("disabled", false);
@@ -36,17 +37,18 @@ dayBtn.on("click", function(event){
 
     themeBtnDiv.css("background", "rgb(75, 121, 177)");
     footerDiv.css("background", "#95E4EC");
-    bannerDiv.css("background", "#95E4EC");
     dawnBtn.attr("disabled", true);
     duskBtn.attr("disabled", true);
     darkBtn.attr("disabled", true);
     doomBtn.attr("disabled", true);
 
     background.attr("class", "day");   
+    bannerDiv.attr("class", "day-outer");
     setTimeout(function(){
         background.attr("style", 
-        "background: url('./assets/images/general/bg-day.png'); background-size: contain; background-repeat: no-repeat; width: 100%; height: 0; padding-top: 75%;"
+        "background: url('./assets/images/general/bg-day.png'); background-size: contain; background-repeat: no-repeat; width: 80%; height: 0; padding-top: 60%;"
         );
+        bannerDiv.css("background", "#95E4EC");
         dawnBtn.attr("disabled", false);
         duskBtn.attr("disabled", false);
         darkBtn.attr("disabled", false);
@@ -59,17 +61,18 @@ duskBtn.on("click", function(event){
 
     themeBtnDiv.css("background", "rgb(225, 153, 45)");
     footerDiv.css("background", "#FFE76C");
-    bannerDiv.css("background", "#FFE76C");
     dawnBtn.attr("disabled", true);
     dayBtn.attr("disabled", true);
     darkBtn.attr("disabled", true);
     doomBtn.attr("disabled", true);
 
     background.attr("class", "dusk");
+    bannerDiv.attr("class", "dusk-outer");
     setTimeout(function(){
         background.attr("style", 
-        "background: url('./assets/images/general/bg-dusk.png'); background-size: contain; background-repeat: no-repeat; width: 100%; height: 0; padding-top: 75%;"
+        "background: url('./assets/images/general/bg-dusk.png'); background-size: contain; background-repeat: no-repeat; width: 80%; height: 0; padding-top: 60%;"
         );
+        bannerDiv.css("background", "#FFE76C");
         dawnBtn.attr("disabled", false);
         dayBtn.attr("disabled", false);
         darkBtn.attr("disabled", false);
@@ -82,17 +85,18 @@ darkBtn.on("click", function(event){
 
     themeBtnDiv.css("background", "rgb(124, 125, 171)");
     footerDiv.css("background", "#08366D");
-    bannerDiv.css("background", "#08366D");
     dawnBtn.attr("disabled", true);
     duskBtn.attr("disabled", true);
     dayBtn.attr("disabled", true);
     doomBtn.attr("disabled", true);
 
     background.attr("class", "dark");
+    bannerDiv.attr("class", "dark-outer");
     setTimeout(function(){
         background.attr("style", 
-        "background: url('./assets/images/general/bg-dark.png'); background-size: contain; background-repeat: no-repeat; width: 100%; height: 0; padding-top: 75%;"
+        "background: url('./assets/images/general/bg-dark.png'); background-size: contain; background-repeat: no-repeat; width: 80%; height: 0; padding-top: 60%;"
         );
+        bannerDiv.css("background", "#08366D");
         dawnBtn.attr("disabled", false);
         duskBtn.attr("disabled", false);
         dayBtn.attr("disabled", false);
@@ -105,17 +109,18 @@ doomBtn.on("click", function(event){
 
     themeBtnDiv.css("background", "rgb(69, 67, 67)");
     footerDiv.css("background", "#BDA5A0");
-    bannerDiv.css("background", "#BDA5A0");
     dawnBtn.attr("disabled", true);
     duskBtn.attr("disabled", true);
     dayBtn.attr("disabled", true);
     darkBtn.attr("disabled", true);
 
     background.attr("class", "doom");
+    bannerDiv.attr("class", "doom-outer");
     setTimeout(function(){
         background.attr("style", 
-        "background: url('./assets/images/general/bg-doom.png'); background-size: contain; background-repeat: no-repeat; width: 100%; height: 0; padding-top: 75%;"
+        "background: url('./assets/images/general/bg-doom.png'); background-size: contain; background-repeat: no-repeat; width: 80%; height: 0; padding-top: 60%;"
         );
+        bannerDiv.css("background", "#BDA5A0");
         dawnBtn.attr("disabled", false);
         duskBtn.attr("disabled", false);
         dayBtn.attr("disabled", false);
